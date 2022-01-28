86b21e0f90499c65b2643b246789dca804725d1c
dec1be1 <ju@chezju.org>
2022-01-28 22:04:29 +0100

    borg backup example script updated

--- Aides-memoires/borg/borg_backup.sh
+++ Aides-memoires/borg/borg_backup.sh
@@ -2,32 +2,19 @@
 
 # some parameters
 PATHS_TO_BACKUP="/firstpath /secondpath"
-PATHS_TO_EXCLUDE="--exclude /firstpath/.cache --exclude /secondpath/.cache"
 BORG_REPO="ssh://username@hostname/path/to/repo/repo_name"
+EXCLUDE_FILE="/path/to/exlude/file/borg.exclude"
 BORG_ARCHIVE_PATTERN="{hostname}_backup_name_{utcnow}"
+export BORG_PASSPHRASE="mysecurebackuppassphrase"
 
 # set local tmp folder on a large capacity partition
-#export BORG_CACHE_DIR=/mnt/SAV/borg/tmp
+# export BORG_CACHE_DIR=/mnt/borg/tmp
 
 # paths
 ECHO="$(which echo)"
 BORG="$(which borg)" ||{ ${ECHO} "[!] borg not found. Bye!"; exit 1; }
 
-# start backup
-${ECHO} "[+] Starting backup of ${PATHS_TO_BACKUP} to ${BORG_REPO}..."
-
-${BORG} create \
-        --stats \
-        --progress \
-        --show-rc \
-        --umask 0007 \
-        --compression zstd,10 \
-        --exclude-caches \
-        ${PATHS_TO_EXCLUDE} \
-        ${BORG_REPO}::${BORG_ARCHIVE_PATTERN} \
-        ${PATHS_TO_BACKUP}
-
-# pruning repo
+# prune repo
 ${ECHO} "[+] Pruning ${BORG_REPO}..."
 ${BORG} prune \
         --stats \
@@ -38,4 +25,24 @@ ${BORG} prune \
         --keep-yearly=-1 \
         ${BORG_REPO}
 
+# start backup
+${ECHO} "[+] Starting backup of ${PATHS_TO_BACKUP} to ${BORG_REPO}..."
+${BORG} create \
+        --stats \
+        --progress \
+        --show-rc \
+        --umask 0007 \
+        --compression zstd,10 \
+        --exclude-caches \
+        --exclude-from ${EXCLUDE_FILE} \
+        ${BORG_REPO}::${BORG_ARCHIVE_PATTERN} \
+        ${PATHS_TO_BACKUP}
+
+# archives integrity check
+${ECHO} "[+] Checking archives integrity..."
+${BORG} check \
+        --archives-only \
+        ${BORG_REPO}
+
+${ECHO} "[*] Borg backup finished. Bye!"
 exit 0
